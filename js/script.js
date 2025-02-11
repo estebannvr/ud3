630cd616ad47bb4333e972fa2fb3e68d00a7f50f
estebannvr <navarroesteban272@gmail.com>
2025-02-11 14:47:13 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-function mostrarMensaje(){
-	let num1;
-	let num2;
-	num1 = document.getElementById("num1").value;
-	num2 = document.getElementById("num2").value;
-	document.getElementById("mensaje").innerHTML = "El primer número es:" + num1 + "<br>El segundo numero es: " + num2 + "<br>La suma es: " + (num1+num2) + "<br> La resta es: "  + (num1-num2) + "<br> El producto es: " + (num1*num2) + "<br> La division es : " + (num1/num2) + "<br>El resto es: " + (num1%num2) 
-}
+function comprobar(){
+	let num;
+	let resto;
+	num = document.getElementById("num").value;
+	resto = num % 2;
+	if (resto == 1){
+		
+		document.getElementById("mensaje").innerHTML = "El numero es impar"
+			
+	} else {
+		
+		document.getElementById("mensaje").innerHTML = "El numero es par"
+	}	
+}	

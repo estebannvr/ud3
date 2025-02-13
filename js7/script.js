e45eb46289d26ccf9746c6a10f35e3a13111c78b
estebannvr <navarroesteban272@gmail.com>
2025-02-13 12:25:14 +0100

    Add files via upload

---
+++ js7/script.js
@@ -0,0 +1,14 @@
+function comprobar(){
+	let nota;
+	nota < 10
+	
+	nota = document.getElementById("nota").value;
+	if (nota < 5){
+		
+		document.getElementById("mensaje").innerHTML = "Suspenso"
+			
+	} else {
+		
+		document.getElementById("mensaje").innerHTML = "Aprobado"
+	}	
+}	

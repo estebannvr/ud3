befd041b45529bff8041282c648c47165cf74a92
estebannvr <navarroesteban272@gmail.com>
2025-03-20 17:01:15 +0100

    Add files via upload

---
+++ act10/js/script.js
@@ -0,0 +1,6 @@
+function Array(){
+	let vector = ["Carlos","Juan","Paco"];
+	vector[0] = prompt("Introduce la nota de Carlos:");
+	vector[1] = prompt("Introduce la nota de Juan:");
+	vector[2] = prompt("Introduce la nota de Paco:");
+}
